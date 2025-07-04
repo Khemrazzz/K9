8dc253b5392ef67dfc0a65dd49113453dae067d0
Shar5K_95 <145230570+Khemrazzz@users.noreply.github.com>
2025-07-05 00:57:02 +0400

    2.0

--- HackerPlatform-Backend/src/main/java/com/myorg/hackerplatform/HackerPlatform/jwt/JwtUtil.java
+++ HackerPlatform-Backend/src/main/java/com/myorg/hackerplatform/HackerPlatform/jwt/JwtUtil.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
