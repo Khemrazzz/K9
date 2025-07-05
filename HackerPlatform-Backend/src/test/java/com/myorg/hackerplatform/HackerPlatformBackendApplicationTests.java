47d8fa169a6a819635a390da8e539378cec7a7e0
Shar5K_95 <145230570+Khemrazzz@users.noreply.github.com>
2025-07-05 04:02:45 +0400

    Add UserDetailsService and password encoding

--- HackerPlatform-Backend/src/test/java/com/myorg/hackerplatform/HackerPlatformBackendApplicationTests.java
+++ HackerPlatform-Backend/src/test/java/com/myorg/hackerplatform/HackerPlatformBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.myorg.hackerplatform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "JWT_SECRET=dummy")
 class HackerPlatformBackendApplicationTests {
 
 	@Test
